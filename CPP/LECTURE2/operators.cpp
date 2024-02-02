6d2e6d8d5b0827b89fcc976c342e4a724d59623b
Parasjmail <paras.jmail1234@gmail.com>
2024-02-02 13:35:04 +0530

    new commit

--- CPP/LECTURE2/operators.cpp
+++ CPP/LECTURE2/operators.cpp
@@ -9,5 +9,29 @@ int main(){
 
     std::cout << 2.0/5 << std::endl;
 
+    //Relational operator
+
+    int p = 2;
+    int q = 3;
+    std::cout << std::boolalpha;
+
+    bool first =  (p==q);
+    std::cout << first <<std::endl;
+
+    bool second =  (p<=q);
+    std::cout << second <<std::endl;
+
+    bool third =  (p>=q);
+    std::cout << third <<std::endl;
+
+    bool fourth =  (p!=q);
+    std::cout << fourth <<std::endl;
+
+    std::cout << std::endl;
+    std::cout << std::noboolalpha;
+    //Logical
+    int l = 0;
+    std::cout << !l <<std::endl;
+
 
 }
