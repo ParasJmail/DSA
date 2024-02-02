52657f34503df702c2506d984ed7b0e6af34869b
Parasjmail <paras.jmail1234@gmail.com>
2024-02-02 13:12:01 +0530

    new commits

---
+++ CPP/LECTURE2/DatatypesAndVariable.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(){
+    int a =123;
+
+    std::cout << a << std::endl;
+    char b = 'v';
+
+    std::cout << b << std::endl;
+
+    bool bl = true;
+    std::cout << bl << std::endl;
+
+    float f = 1.2;
+    std::cout << f << std::endl;
+
+    double d = 1.23;
+    std::cout << d << std::endl;
+
+    int size = sizeof(d);
+    std::cout << size << std::endl;
+}
---
+++ CPP/LECTURE2/typecasting.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    int a = 'a';
+
+    std::cout << a << std::endl;
+
+    char ch = 98;
+    std::cout << ch << std::endl;
+}
