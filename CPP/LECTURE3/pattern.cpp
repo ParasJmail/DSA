11ce82feedcd86947bfce12f23f4d3eb4012bc2d
Parasjmail <paras.jmail1234@gmail.com>
2024-02-07 13:53:29 +0530

    new commit

---
+++ CPP/LECTURE3/pattern.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+    int n{};
+    std::cin >> n;
+
+    int i{1};
+
+    while(i <=n){
+        int j{1};
+        while (j<=n)
+        {
+            std::cout <<"*";
+            j++;
+        }
+        std::cout<<std::endl;
+        i++;
+        
+    }
+}
