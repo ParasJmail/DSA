0dc804b50d7b33177916e4f2acb868e935d0ce35
Parasjmail <paras.jmail1234@gmail.com>
2024-02-24 09:39:50 +0530

    new commit

--- CPP/2BINARY_SEARCH/LECTURE13/binarysearch2.cpp
+++ CPP/2BINARY_SEARCH/LECTURE13/binarysearch2.cpp
@@ -1,77 +1,56 @@
-// First and Last Position of an element in an srted array.
+#include<vector>
+using namespace std;
+int firstOcc(vector<int>& arr, int n, int key) {
 
-#include <iostream>
-
-int firstOccurence(int arr[] , int size , int key){
-
-    int start {0};
-    int end {size -1};
-    int mid {start + (end - start)/2};
-    int ans {-1};
-
-    while(start <= end){
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e) {
 
         if(arr[mid] == key){
             ans = mid;
-            end = mid -1;
+            e = mid - 1;
         }
-        else if (key > arr[mid]) //Right
-        {
-            start = mid + 1;
+        else if(key > arr[mid]) {//Right me jao
+            s = mid + 1;
         }
-        else if (key < arr[mid]) //Left
-        {
-             end = mid - 1;
+        else if(key < arr[mid]) {//left me jao
+            e = mid - 1;
         }
-        
-        mid = start + (end-start)/2;
 
+        mid = s + (e-s)/2;
     }
-
     return ans;
 }
 
-int lasttOccurence(int arr[] , int size , int key){
+int lastOcc(vector<int>& arr, int n, int key) {
 
-    int start {0};
-    int end {size -1};
-    int mid {start + (end - start)/2};
-    int ans {-1};
-
-    while(start <= end){
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e) {
 
         if(arr[mid] == key){
             ans = mid;
-            start = mid +1;
+            s = mid + 1;
         }
-        else if (key > arr[mid]) //Right
-        {
-            start = mid + 1;
+        else if(key > arr[mid]) {//Right me jao
+            s = mid + 1;
         }
-        else if (key < arr[mid]) //Left
-        {
-             end = mid - 1;
+        else if(key < arr[mid]) {//left me jao
+            e = mid - 1;
         }
-        
-        mid = start + (end-start)/2;
 
+        mid = s + (e-s)/2;
     }
-
     return ans;
 }
 
-
-
-
-int main(){
-
-    int arr[6] {0,5,5,6,6,6};
-
-    int fst = firstOccurence(arr ,6,6); 
-    int lst = lasttOccurence(arr ,6,5); 
-
-    std::cout << fst << std::endl;
-    std::cout << lst << std::endl;
-
-    return 0;
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
+{
+    pair<int,int> p;
+    p.first = firstOcc(arr, n, k);
+    p.second = lastOcc(arr, n, k);
+    
+    return p;
 }
